e41da51a07755fe8effc928c53c335306ccc1c16
Jagadeesh J <jagadeeshj@ideas2it.com>
2023-01-06 23:46:35 +0530

    fix: app.py
    Signed-off-by: Jagadeesh J <jagadeeshj@ideas2it.com>

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 import openai
-from flask import Flask, request
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -22,6 +22,21 @@ def index():
         stop=[" Human:", " AI:"]
     )
     return response.choices[0].text
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(animal),
+            temperature=0.6,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
 
 
 def generate_prompt(query):
