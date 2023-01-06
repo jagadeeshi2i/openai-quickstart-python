011271434a56bd955666569ea150385e16067590
Jagadeesh J <jagadeeshj@ideas2it.com>
2023-01-06 23:03:15 +0530

    change app for vercel
    Signed-off-by: Jagadeesh J <jagadeeshj@ideas2it.com>

--- app.py
+++ app.py
@@ -1,35 +1,33 @@
 import os
 
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, request
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route("/", methods=("GET", "POST"))
+@app.route("/search", methods=["GET"])
 def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+    args = request.args
+    query = args.get('query')
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=generate_prompt(query),
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
     )
+    return response.choices[0].text
+
+
+def generate_prompt(query):
+    return """The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.
+
+    Human: Hello, who are you?
+    AI: I am an AI created by OpenAI. How can I help you today?
+    Human: {}.
+    AI: """.format(query)
